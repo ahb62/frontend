3f8916bda4675cf2786b197d6401c1a99825cd98
ahb.62 <ahb.62@protonmail.com>
2023-02-19 21:42:40 -0400

    displaying table with all data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 import { Student } from './models/students.model';
 @Component({
   selector: 'app-root',
@@ -7,18 +6,7 @@ import { Student } from './models/students.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private http: HttpClient) {}
-  students: Student[] = [];
-  students_active: any;
-  
-  ngOnInit() {
-    this.http.get<Student[]>("http://localhost:3000/characters").subscribe((data: Student[]) => {
-      console.log(data);
-      this.students = data;
-      const students_Active: Student[] = this.students.filter((student) => student.active);
-      this.students_active = students_Active;
-      console.log(students_Active);
-    });
+
 
 }
-}
+
--- src/app/components/students/students.component.ts
+++ src/app/components/students/students.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 import { Student } from '../../models/students.model';
 @Component({
   selector: 'app-students',
@@ -6,31 +7,42 @@ import { Student } from '../../models/students.model';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent {
+  constructor(private http: HttpClient) {}
+
   displayedColumns: string[] = ['ID', 'Active', 'Grades', 'Name', 'Gender', 'Class', 'Club', 'Persona', 'BreastSize', 'Strength', 'Hairstyle', 'Color', 'Stockings', 'Accessory'];
 
-  @Input() studentsActive: Student = {
-    ID: 0,
-    active: false,
-    grades: 0,
-    Name: "",
-    Gender: 0,
-    Class: 0,
-    Club: 0,
-    Persona: 0,
-    Crush: 0,
-    BreastSize: 0,
-    Strength: 0,
-    Hairstyle: 0,
-    Color: "",
-    Stockings: "",
-    Accessory: 0,
-      ScheduleTime: "",
-      ScheduleDestination: "",
-      ScheduleAction: ""
+  // @Input() studentsActive: Student = {
+  //   ID: 0,
+  //   active: false,
+  //   grades: 0,
+  //   Name: "",
+  //   Gender: 0,
+  //   Class: 0,
+  //   Club: 0,
+  //   Persona: 0,
+  //   Crush: 0,
+  //   BreastSize: 0,
+  //   Strength: 0,
+  //   Hairstyle: 0,
+  //   Color: "",
+  //   Stockings: "",
+  //   Accessory: 0,
+  //     ScheduleTime: "",
+  //     ScheduleDestination: "",
+  //     ScheduleAction: ""
     
-  };
-  consoleando() {  
-    console.log(this.studentsActive);
+  // };
+
+  students: Student[] = [];
+  students_active: any;
+  dataSource: any;
+  ngOnInit(): void {
+    this.http.get<Student[]>("http://localhost:3000/characters").subscribe((data: Student[]) => {
+      console.log(data);
+      this.students = data;
+      const students_Active: Student[] = this.students.filter((student) => student.active);
+      this.students_active = students_Active;
+      this.dataSource = this.students_active;
+    });
   }
 }
-
